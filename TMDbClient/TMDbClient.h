c14008a996bd9c4209ccc80dffe13c1cfc0ab953
Oli Pfeffer <oliver.pfeffer@gmail.com>
2016-12-07 19:55:32 -0500

    Initial Commit

---
+++ TMDbClient/TMDbClient.h
@@ -0,0 +1,19 @@
+//
+//  TMDbClient.h
+//  TMDbClient
+//
+//  Created by Oliver Pfeffer on 12/7/16.
+//  Copyright © 2016 Astrio, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDbClient.
+FOUNDATION_EXPORT double TMDbClientVersionNumber;
+
+//! Project version string for TMDbClient.
+FOUNDATION_EXPORT const unsigned char TMDbClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDbClient/PublicHeader.h>
+
+
